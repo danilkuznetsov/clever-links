003316c17b05b3e28f5c5a9ee9436609b8f59d13
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-02-07 22:19:29 +0200

    add in indexcontroller redirectToLongUrl method and some test

--- src/main/java/io/github/danilkuznetsov/urlshortener/controller/IndexController.java
+++ src/main/java/io/github/danilkuznetsov/urlshortener/controller/IndexController.java
@@ -1,8 +1,12 @@
 package io.github.danilkuznetsov.urlshortener.controller;
 
+import io.github.danilkuznetsov.urlshortener.service.UrlShorterService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.inject.Inject;
 
 /**
  * Created by danil.kuznetsov on 31/01/17.
@@ -11,10 +15,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class IndexController {
 
+    @Inject
+    private UrlShorterService urlService;
+
     @RequestMapping("/")
     @ResponseBody
-    public String index(){
+    public String index() {
         return "Hello world! I'm url shortener service";
+    }
+
+    @RequestMapping("/{id}")
+    public String redirectToLongUrl(@PathVariable("id") String shortUrl) {
+        String longUrl = urlService.findLongUrlByShortUrl(shortUrl);
+        if ((longUrl != null) && (!longUrl.isEmpty())) {
+            return "redirect:" + longUrl;
+        }
+        return "redirect:/";
     }
 
 
--- src/test/java/io/github/danilkuznetsov/urlshortener/controller/IndexControllerUnitTest.java
+++ src/test/java/io/github/danilkuznetsov/urlshortener/controller/IndexControllerUnitTest.java
@@ -1,16 +1,19 @@
 package io.github.danilkuznetsov.urlshortener.controller;
 
+import io.github.danilkuznetsov.urlshortener.service.UrlShorterService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.inject.Inject;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Created by danil.kuznetsov on 07/02/17.
@@ -21,11 +24,48 @@ public class IndexControllerUnitTest {
     @Inject
     MockMvc mockMvc;
 
+    @MockBean
+    UrlShorterService shorterService;
+
     @Test
     public void shouldReturnHello() throws Exception {
-        this.mockMvc
+        //give
+        String expectedContent = "Hello world! I'm url shortener service";
+        //when and then
+        mockMvc
                 .perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello world! I'm url shortener service"));
+                .andExpect(content().string(expectedContent));
+    }
+
+    @Test
+    public void shouldRedirectToLongUrlByShortId() throws Exception {
+        //give
+        String shortUrls = "shortUrls";
+        String longUrl = "http://gmail.com";
+
+        given(shorterService.findLongUrlByShortUrl(shortUrls)).willReturn(longUrl);
+        //when and then
+        mockMvc
+                .perform(get("/{id}", shortUrls))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(longUrl));
+
+        verify(shorterService).findLongUrlByShortUrl(shortUrls);
+    }
+
+    @Test
+    public void shouldReturnNotFoundByShortIdIsNotFound() throws Exception {
+        //give
+        String shortUrls = "shortUrls";
+
+        given(shorterService.findLongUrlByShortUrl(shortUrls)).willReturn(null);
+        //when and then
+        mockMvc
+                .perform(get("/{id}", shortUrls))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+
+        verify(shorterService).findLongUrlByShortUrl(shortUrls);
     }
 }
