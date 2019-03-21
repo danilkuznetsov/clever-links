eca71bea069800563390cbcb75af224df1a477ef
Danil Kuznetsov <kuznetsov.danil.v@gmail.com>
2019-03-21 12:39:07 +0200

    fix broken tests

--- src/test/resources/datasets/fullUrls/oneFullUrl.sql
+++ src/test/resources/datasets/fullUrls/oneFullUrl.sql
@@ -1,3 +1,6 @@
+delete from short_url;
+delete from full_url;
+
 insert into  full_url (id, url) values (1, 'http://google.com');
 
 insert into short_url(id, url, full_url_id) values (1, 'http://s.com/gl',1);
