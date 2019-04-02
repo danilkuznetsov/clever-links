d2cd9d9c4e89b9199c96f5d57755d0499dc04029
Danil Kuznetsov <kuznetsov.danil.v@gmail.com>
2019-04-02 18:59:26 +0300

    fix demo data fix tests datasets

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 insert into full_url  (id, url) values (1, 'http://google.com');
-insert into short_url (url, full_url_id) values ('gl', 1);
+insert into short_url (url, full_url_id, enabled) values ('gl', 1, true);
--- src/test/resources/datasets/fullUrls/twoUrlWithTwoShortUrls.sql
+++ src/test/resources/datasets/fullUrls/twoUrlWithTwoShortUrls.sql
@@ -3,9 +3,9 @@ delete from full_url;
 
 insert into  full_url (id, url) values (1, 'http://google.com');
 
-insert into short_url(id, url, full_url_id) values (1, 'http://s.com/gl1',1);
-insert into short_url(id, url, full_url_id) values (2, 'http://s.com/gl11',1);
+insert into short_url(id, url, full_url_id, enabled) values (1, 'http://s.com/gl1',1, true);
+insert into short_url(id, url, full_url_id, enabled) values (2, 'http://s.com/gl11',1, true);
 
 insert into  full_url (id, url) values (2, 'http://google.com2');
-insert into  short_url(id, url, full_url_id) values (3, 'http://s.com/gl2',2);
-insert into  short_url(id, url, full_url_id) values (4, 'http://s.com/gl22',2);
+insert into  short_url(id, url, full_url_id, enabled) values (3, 'http://s.com/gl2',2, true);
+insert into  short_url(id, url, full_url_id, enabled) values (4, 'http://s.com/gl22',2, true);
